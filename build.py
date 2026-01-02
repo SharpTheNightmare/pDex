a9e64ae4f79ab0ef8964eaa4940609d912e762b9
SharpTheNightmare <koen.t04@gmail.com>
2026-01-02 05:35:15 -0600

    Update build.py

--- build.py
+++ build.py
@@ -1,32 +1,34 @@
 import os
 
-headerText = open("header.lua","r")
-embedStr = headerText.read() + "\n\nlocal EmbeddedModules = {\n"
-headerText.close()
+def readfile(path):
+    with open(path, "r") as file:
+        return file.read()
+
+# Read version from VERSION file
+version = readfile("VERSION").strip()
+
+headerText = readfile("header.lua")
+embedStr = headerText + "\n\nlocal EmbeddedModules = {\n"
 
 files = os.listdir("modules")
 
-
-def readfile(path):
-    file = open(path,"r")
-    str = file.read()
-    file.close()
-    return str
-
 def addModuleFile(path):
     global embedStr
-
     moduleName = os.path.splitext(os.path.basename(path))[0]
     moduleSource = readfile(path)
-
     embedStr = embedStr + '["' + moduleName + '"] = function()\n' + moduleSource + '\nend,\n'
 
 for filename in files:
     addModuleFile("modules/" + filename)
 
 embedStr = embedStr + "}"
-embedStr = embedStr + "\n" + readfile("main.lua")
 
-file = open("out.lua","w")
-file.write(embedStr)
-file.close()
+# Read main.lua and inject version
+mainLua = readfile("main.lua")
+mainLua = mainLua.replace('"{{VERSION}}"', f'"{version}"')
+embedStr = embedStr + "\n" + mainLua
+
+with open("pDex.lua", "w") as file:
+    file.write(embedStr)
+
+print(f"Built pDex.lua with version {version}")
